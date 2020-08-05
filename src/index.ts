142d052b4ce977e6076e75d64df3297bf572b897
vapvin <vins@vins.dev>
2020-08-05 23:27:37 +0900

    Devs Express Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Test Server");
+import { Options } from 'graphql-yoga';
+import app from './app';
+
+const PORT: number | string = process.env.PORT || 4444;
+const PLAYGROUND_ENDPOINT: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/graphql";
+
+const appOptions: Options = {
+    port: PORT,
+    playground: PLAYGROUND_ENDPOINT,
+    endpoint: GRAPHQL_ENDPOINT
+}
+
+const startServer = () => console.log(`Start Server on port http://127.0.0.1:${PORT}`)
+
+app.start(appOptions, startServer);
